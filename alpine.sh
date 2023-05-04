557513dc7468a64ce8d32877fd4fb43782b701b0
ukhu <ukhu>
2023-05-04 16:50:55 -0500

    service auto start

--- alpine.sh
+++ alpine.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 apk add --update git python3 python3-dev vim make automake gcc g++ subversion
 python3 -m ensurepip
+cp attendance /ect/init.d/
+rc-update add attendance
+rc-service attendace start
