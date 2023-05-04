1f1078bb15d726dd1ed8a9b8f4cb01a9f0b52dd0
ukhu <ukhu>
2023-05-04 16:52:23 -0500

    correction

--- alpine.sh
+++ alpine.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apk add --update git python3 python3-dev vim make automake gcc g++ subversion
 python3 -m ensurepip
-cp attendance /ect/init.d/
+cp attendance /etc/init.d/
 rc-update add attendance
 rc-service attendace start
