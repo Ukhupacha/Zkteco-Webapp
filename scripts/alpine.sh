a7a0b2e50804a5400997d62327838d3df23bd946
ukhu <ukhu>
2023-05-05 11:57:55 -0500

    tiny error

--- scripts/alpine.sh
+++ scripts/alpine.sh
@@ -2,6 +2,6 @@
 apk add --update git python3 python3-dev vim make automake gcc g++ subversion
 python3 -m ensurepip
 pip3 install -r ../requirements.txt
-cp ../attendance /etc/init.d/
+cp attendance /etc/init.d/
 rc-update add attendance
 rc-service attendance start
