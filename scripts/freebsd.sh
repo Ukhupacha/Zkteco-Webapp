753faa0af0f7df8470d9492cbfbbe289c9514831
Ukhu <Ukhu>
2023-05-08 12:28:17 -0500

    update

--- scripts/freebsd.sh
+++ scripts/freebsd.sh
@@ -5,25 +5,14 @@ pip install -r ../requirements.txt
 cat > /etc/rc.d/attendance << EOF
 #!/bin/sh
 
-# PROVIDE: attendance
-# REQUIRE:
-# KEYWORD: shutdown
-
 . /etc/rc.subr
 
 name=attendance
 rcvar=attendance_enable
 
+command="/usr/sbin/\${name}"
+
 load_rc_config \$name
-
-: ${attendance_enable="NO"}
-: ${attendance_home_dir:="/root/Zkteco"}
-
-pidfile="/var/run/\${name}.pid"
-procname="python /root/Zkteco/app.py"
-command=/usr/sbin/daemon
-command_args="-f -p \${pidfile} -u attendance \${procname} --home=\${attendance_home_dir} --logfile=default"
-
 run_rc_command "\$1"
 EOF
 
