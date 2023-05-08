6a8ccf885533e6161980cd63fb3ae94e7c75fe66
Ukhu <Ukhu>
2023-05-08 12:07:13 -0500

    update script

--- scripts/freebsd.sh
+++ scripts/freebsd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pkg install -y python py39-pip py39-matplotlib
+pkg install -y python py39-pip py39-matplotlib vim
 pip install -r ../requirements.txt
 
 cat > /etc/rc.d/attendance << EOF
@@ -16,14 +16,14 @@ rcvar=attendance_enable
 
 command="/root/Zkteco/python app.py"
 
-load_rc_config $name
+load_rc_config \$name
 
 #
 # DO NOT CHANGE THESE DEFAULT VALUES HERE
 # SET THEM IN THE /etc/rc.conf FILE
 #
-attendance_enable=${attendance_enable-"NO"}
-pidfile=${attendance_pidfile-"/var/run/attendance.pid"}
+attendance_enable=\${attendance_enable-"NO"}
+pidfile=\${attendance_pidfile-"/var/run/attendance.pid"}
 
 run_rc_command "\$1"
 EOF
