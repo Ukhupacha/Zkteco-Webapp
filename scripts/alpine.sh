d3a0a7ab222ea878142cde273a47c4cea3c4c473
Ukhu <Ukhu>
2023-05-08 10:08:44 -0500

    move service under alpine.sh

--- scripts/alpine.sh
+++ scripts/alpine.sh
@@ -2,6 +2,16 @@
 apk add --update git python3 python3-dev vim make automake gcc g++ subversion
 python3 -m ensurepip
 pip3 install -r ../requirements.txt
-cp attendance /etc/init.d/
+cact >  /etc/init.d/attendance << EOF
+#!/sbin/openrc-run
+# Copyright 2021 Gentoo Authors
+# Distributed under the terms of the GNU General Public License v2
+
+name="attendance"
+description="Attendance zkteco"
+command="/root/Zkteco/app.py"
+pidfile="/run/${RC_SVCNAME}.pid"
+command_background=true
+EOF
 rc-update add attendance
 rc-service attendance start
