1e90293d6212aba7e9df202cbca46138f7bd7ce8
Ukhu <Ukhu>
2023-05-09 10:25:49 -0500

    update with supreviseror

--- scripts/freebsd.sh
+++ scripts/freebsd.sh
@@ -1,35 +1,11 @@
 #!/bin/sh
-pkg install -y python py39-pip py39-matplotlib vim
+pkg install -y python py39-pip py39-matplotlib vim sysutils/py-supervisor
 pip install -r ../requirements.txt
-
-cat > /usr/local/etc/rc.d/attendance << EOF
-
-#!/bin/sh
-
-# PROVIDE: attendance
-# REQUIRE: LOGIN
-# KEYWORD: shutdown
-
-. /etc/rc.subr
-
-name=attendance
-rcvar=\`set_rcvar\`
-
-load_rc_config \$name
-
-: \${attendance_enable="NO"}
-: \${attendance_home_dir:="/root/Zkteco/"}
-
-pidfile="/var/run/\${name}.pid"
-attendance_user="root"
-procname="python /root/Zkteco/app.py"
-command=/usr/sbin/daemon
-command_args="-c -f -P \${pidfile} -u \${attendance_user} -r \${procname} --home=\${attendance_home_dir} --logfile=default"
-
-run_rc_command "\$1"
-
-EOF
-
-chmod +x /usr/local/etc/rc.d/attendance
-service attendance enable
-service attendance start
+# possibility 2:
+echo "[program:attendance]
+command=/root/Zkteco/app.py
+directory=/root/Zkteco
+autostart=true
+" >> /usr/local/etc/supervisord.conf
+service supervisord enable
+service supervisord start
